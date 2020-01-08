f25e162f1f43cc9abf9914c3409aca6a02347039
OoDone <48130949+OoDone@users.noreply.github.com>
2020-01-08 14:16:10 -0500

    Update test.js

--- test.js
+++ test.js
@@ -8,4 +8,7 @@ search.search().then(results=>{
   var results2 = JSON.stringify(results);
   //console.log(results['title']);
   console.log(results2);
+  var dat = JSON.parse(results2);
+  var correct = dat['questions']['0']['choices']['0']['correct'];
+  console.log(correct);
 });
