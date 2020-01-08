36cea5a2a05a3b8a56d0bbbf7da17f6e9edced92
OoDone <48130949+OoDone@users.noreply.github.com>
2020-01-08 15:22:02 -0500

    Update test.js

--- test.js
+++ test.js
@@ -9,6 +9,17 @@ search.search().then(results=>{
   //console.log(results['title']);
   //console.log(results2);
   var dat = JSON.parse(results2);
+  let configKeys = Object.keys(dat.questions);
+configKeys.forEach((rand)=>{
+	console.log(rand);
+	var itemKeys = Object.keys(dat[rand]);
+  console.log(itemKeys);
+  for(var i=0;i<itemKeys.length;i++){
+  	let randName = itemKeys[i];
+  	console.log(config.configuration[rand][randName]['choices']);
+    //console.log(config.configuration[rand][randName]['key2']);
+  }
+});
   var correct = dat['Language'];
   //['0']['choices']['0']['correct'];
   console.log(correct + " X D " + results['Language'] + " " + results.uuid);
