0159f0f40d11b667d8218c007f730f0b81a7c715
OoDone <48130949+OoDone@users.noreply.github.com>
2020-01-08 15:34:31 -0500

    Update test.js

--- test.js
+++ test.js
@@ -9,17 +9,7 @@ search.search(o=>{
   //console.log(results['title']);
   //console.log(results2);
   var dat = JSON.parse(results2);
-  var correct = Object.keys(dat);
   var results = dat.kahoot.title;
   console.log(results);
-  console.log(correct);
 }).then(results=>{
-  var results2 = JSON.stringify(results);
-  //console.log(results['title']);
-  //console.log(results2);
-  var dat = JSON.parse(results2);
-  var correct = Object.keys(dat);
-  console.log(correct);
-  //['0']['choices']['0']['correct'];
-  //console.log(correct + " X D " + results['Language'] + " " + results.uuid);
 });
