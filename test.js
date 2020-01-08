44ebbf2614532f7fce36c9111390f0b7d954036c
OoDone <48130949+OoDone@users.noreply.github.com>
2020-01-08 15:32:06 -0500

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,16 @@ const config = {
   questionLength: 9
 };
 const search = new Searcher("January", config);
-search.search().then(results=>{
+search.search(o=>{
+  var results2 = JSON.stringify(results);
+  //console.log(results['title']);
+  //console.log(results2);
+  var dat = JSON.parse(results2);
+  var correct = Object.keys(dat);
+  var results = dat.kahoot.title;
+  console.log(results);
+  console.log(correct);
+}).then(results=>{
   var results2 = JSON.stringify(results);
   //console.log(results['title']);
   //console.log(results2);
