cc70e1d0fb1939fbb8dc2f67293a8f337c988f8a
OoDone <48130949+OoDone@users.noreply.github.com>
2020-01-08 13:57:03 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 const Searcher = require("kahoot-search");
 const config = {
-  language: ["es"],
-  questionLength: 9,
+  questionLength: 9
 };
 const search = new Searcher("January", config);
 search.search().then(results=>{
