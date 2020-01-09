e79cb2f83d0df349da9ea14d911f4f3abe1b4372
OoDone <48130949+OoDone@users.noreply.github.com>
2020-01-09 13:51:43 -0500

    Update test.js

--- test.js
+++ test.js
@@ -22,6 +22,6 @@ module.exports.searchs = function (name, type, questioncount, answercount) {
     type: type,
     searchStrictly: true
   };
-  search = new Searcher(name);
+  search = new Searcher(name, config);
   searchk();
 }
